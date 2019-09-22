c0337bc93a259e68a8dadcd977d76b06544c1902
aneme1038 <thriftman25@gmail.com>
2019-09-21 21:11:36 -0400

    controllers setup. server.js updated

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const sessions = require('express-session');
 const cors = require('cors');
 //dotenv
 require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 //Mongoose
 const db = mongoose.connection;
 
@@ -30,11 +31,46 @@ app.use(cors());
 //---------------------
 //CONTROLLERS
 //---------------------
+const userController = require('./src/controllers/users.js');
+app.use('/users', userController);
+const sessionsController = require('./src/controllers/sessions.js');
+app.use('/sessions', sessionsController);
+//Add additional controllers below...
 
+//Main Server Route for user login session
+app.get('/', (req, res) => {
+  if(req.session.currentUser){
+    res.json(req.session.currentUser);
+  } else {
+    res.status(401).json({
+      status: 401,
+      message: "Not Logged In"
+    });
+  }
+});
 //--------------------
 //DATABASE CONNECTION
 //--------------------
-
+//Connect to MongoDB Cloud Atlas
+//add mongodb uri once we have it
+mongoose.connect(/* insert Mongodb uri variable here */, {useNewUrlPaser: true});
+//Error / Success - Development Side
+db.on('error', (error) => {
+  console.log(error.message + ' is MongoD not running?');
+})
+db.on('connected', () => {
+  console.log('Mongo Connected: ', /* insert Mongodb uri variable here */);
+})
+db.on('disconnected', () => {
+  console.log('Mongo Disconnected');
+})
+//Connect to MongoDB locally
+mongoose.connection.once('open', () => {
+  console.log('Connected to Mongoose');
+})
 //--------------------
 //LISTENERS
 //--------------------
+app.listen(PORT, () => {
+  console.log('Listening...');
+})
